4b1c6f7af546ec11fb695438d059fd75da2f9d20
Lennert Verdonck <r0792038@student.thomasmore.be>
2020-10-29 14:25:14 +0100

    ConnectionStringToevoegen

--- Winkellijst_ASP/Startup.cs
+++ Winkellijst_ASP/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Winkellijst_ASP.Data;
 
 namespace Winkellijst_ASP
 {
@@ -24,6 +26,8 @@ namespace Winkellijst_ASP
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<GebruikerContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("GebruikerConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
