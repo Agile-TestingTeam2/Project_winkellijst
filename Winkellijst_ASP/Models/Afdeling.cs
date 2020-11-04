279dd38864aab510c0a7859b19d81b49de1359b1
Stijn <stijn_beck@hotmail.com>
2020-11-04 13:10:08 +0100

    views aanmaken

---
+++ Winkellijst_ASP/Models/Afdeling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Winkellijst_ASP.Models
+{
+    public class Afdeling
+    {
+        public int AfdelingId { get; set; }
+        public string Naam { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public ICollection<AfdelingWinkel> afdelingWinkels { get; set; }
+    }
+}
