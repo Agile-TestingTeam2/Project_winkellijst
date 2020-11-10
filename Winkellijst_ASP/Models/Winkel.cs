c6dfd30e972d9ef9094f27ba63b6c53d3625cfcd
Liedev <60879937+Liedev@users.noreply.github.com>
2020-11-10 19:42:44 +0100

    changed merge

--- Winkellijst_ASP/Models/Winkel.cs
+++ Winkellijst_ASP/Models/Winkel.cs
@@ -13,11 +13,7 @@ namespace Winkellijst_ASP.Models
         public string Straat { get; set; }
         public string Huisnummer { get; set; }
         public string Stad { get; set; }
-<<<<<<< HEAD
-        public ICollection<AfdelingWinkel> AfdelingWinkels { get; set; }
-=======
         public ICollection<Afdeling> Afdelingen{ get; set; }
->>>>>>> develop
         public ICollection<WinkelLijst> WinkelLijsten { get; set; }
 
     }
