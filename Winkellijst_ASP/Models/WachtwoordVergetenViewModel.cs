4fc2a9f34126e321277c3d5560d1f9e4149661a1
Midas Wouters <midas.wouters@gmail.com>
2020-11-19 22:06:41 +0100

    Created basic password reset view

---
+++ Winkellijst_ASP/Models/WachtwoordVergetenViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Winkellijst_ASP.Models
+{
+    public class WachtwoordVergetenViewModel
+    {
+        public string Email { get; set; }
+    }
+}
