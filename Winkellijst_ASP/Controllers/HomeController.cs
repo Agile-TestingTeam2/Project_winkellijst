f67214ee216e246b29a22c6094e150df0a3f0ac9
Midas Wouters <midas.wouters@gmail.com>
2020-11-19 22:05:28 +0100

    Created forgot password controller

--- Winkellijst_ASP/Controllers/HomeController.cs
+++ Winkellijst_ASP/Controllers/HomeController.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Winkellijst_ASP.Models;
+using Winkellijst_ASP.Services;
 
 namespace Winkellijst_ASP.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMailService _mailService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMailService mailService)
         {
             _logger = logger;
+            _mailService = mailService;
         }
 
         public IActionResult Index()
@@ -26,6 +29,24 @@ namespace Winkellijst_ASP.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        // GET: Home/ResetPassword
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        // POST: Home/ResetPassword
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(WachtwoordVergetenViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                await _mailService.SendPasswordResetLink(vm.Email, "Test", "https://google.com");
+            }
+
+            return View(vm);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
