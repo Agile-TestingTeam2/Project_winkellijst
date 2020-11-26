bf09c75f2f0e046fed54e520075589153fa5b380
Liedev <60879937+Liedev@users.noreply.github.com>
2020-11-26 15:59:41 +0100

    scaffolded product and added dummy data

--- Winkellijst_ASP/Controllers/ProductController.cs
+++ Winkellijst_ASP/Controllers/ProductController.cs
@@ -57,7 +57,7 @@ namespace Winkellijst_ASP.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductId,Naam,Prijs,Hoeveelheid,AfdelingId")] Product product)
+        public async Task<IActionResult> Create([Bind("ProductId,Naam,Prijs,Beschrijving,AfdelingId")] Product product)
         {
             if (ModelState.IsValid)
             {
@@ -91,7 +91,7 @@ namespace Winkellijst_ASP.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Naam,Prijs,Hoeveelheid,AfdelingId")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Naam,Prijs,Beschrijving,AfdelingId")] Product product)
         {
             if (id != product.ProductId)
             {
