ca6b78dd5fd26e7f5be1d032a229aaa49a204da9
Liedev <60879937+Liedev@users.noreply.github.com>
2020-12-02 19:04:11 +0100

    validatie gebruikte model en viewmodels. Nog niet af

---
+++ Winkellijst_ASP/Validators/WinkellijstEditViewModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Winkellijst_ASP.ViewModel;
+
+namespace Winkellijst_ASP.Validators
+{
+    public class WinkellijstEditViewModelValidator: AbstractValidator<WinkellijstEditViewModel>
+    {
+        public WinkellijstEditViewModelValidator()
+        {
+            RuleFor(wevm => wevm.Winkellijst.Naam)
+               .NotEmpty()
+               .NotNull()
+               .WithMessage("Gelieve een naam in te vullen");
+        }
+    }
+}
