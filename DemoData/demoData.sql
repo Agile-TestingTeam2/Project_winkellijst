0ea099ddf3f3a104671968881ba391b1cc76c7e2
Liedev <60879937+Liedev@users.noreply.github.com>
2020-12-03 17:13:29 +0100

    Product create,edit,delete,index Views en controller aangemaakt. Viemodels aangemaakt en fluent api controle erop toegepast

--- DemoData/demoData.sql
+++ DemoData/demoData.sql
@@ -5,5 +5,5 @@ Insert into [Winkellijst].[Afdeling]
 ([Naam], [Volgorde], [WinkelId])
 Values ('Verse Producten',('1'),('1')),('Dranken',('2'),('1')),('Zuivel',('3'),('1')),('Diepvries',('4'),('1'));
 Insert into [Winkellijst].[Product]
-([Naam], [AfdelingId], [Hoeveelheid], [Prijs])
-Values('Coca-Cola1.5','2','1.5L','1.63'),('Fanta','2','2L','1.63'),('El gato noir','2','1.5L','4.83'),('Limoen','1','1st','1.63'),('Ice-Tea','2','1.5L','1.63')
+([Naam], [AfdelingId], [Beschrijving], [Prijs])
+Values('Coca-Cola1.5','2','1.5L fles','1.63'),('Fanta','2','2L fles','1.63'),('El gato noir','2','1.5L fles uit het zuiden van Portugal','4.83'),('Limoen','1','Gaat geweldig bij cocktails','1.63'),('Ice-Tea','2','1.5L fles. Koude thee maar dan ook weer niet','1.63')
