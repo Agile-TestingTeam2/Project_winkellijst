52984b915a7e917d21d871a7cb075ea07c1bec29
Lennert Verdonck <r0792038@student.thomasmore.be>
2020-12-09 23:35:22 +0100

    UnitTestingCompleted

--- NUnitTestWinkellijst/UnitOfWorkTests.cs
+++ NUnitTestWinkellijst/UnitOfWorkTests.cs
@@ -3,6 +3,9 @@ using NUnit.Framework;
 using Winkellijst_ASP.Models;
 using System.Collections.ObjectModel;
 using Winkellijst_ASP.UnitOfWork;
+using Winkellijst_ASP.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace NUnitTestWinkellijst
 {
@@ -37,6 +40,32 @@ namespace NUnitTestWinkellijst
             Assert.NotNull(product);
             Assert.IsInstanceOf<Product>(product);
             A.CallTo(() => unitOfWork.ProductRepo.ZoekOpPK(product.ProductId)).MustHaveHappened();
+        }
+        [Test]
+        public void MaakAan_NieuwProductView()
+        {
+            //Arrange
+            ProductViewModel productViewModel;
+            Afdeling afdeling = A.Fake<Afdeling>();
+
+            //Act
+            productViewModel = A.Fake<ProductViewModel>();
+            productViewModel.Product = A.Fake<Product>();
+            productViewModel.Afdeling = new SelectList(unitOfWork.AfdelingRepo.Ophalen(a => afdeling.AfdelingId), "AfdelingIdFake", "NaamFake");
+
+            //Assert
+            Assert.NotNull(productViewModel);
+
+        }
+        [Test]
+        public void CreateWinkelLijst()
+        {
+            //Arrange
+            WinkellijstCreateViewModel viewModel = A.Fake<WinkellijstCreateViewModel>();
+            //Act
+            viewModel.Winkellijst = A.Fake<WinkelLijst>();
+            //Assert
+            Assert.NotNull(viewModel.Winkellijst);
         }
     }
 }
