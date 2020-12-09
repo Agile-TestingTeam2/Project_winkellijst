9202910c08cf00fd8413971769f2cb8cb2a7f1d7
Liedev <60879937+Liedev@users.noreply.github.com>
2020-12-09 19:58:33 +0100

    unittesten voor de dbset unitofwork

---
+++ NUnitTestWinkellijst/UnitOfWorkTests.cs
@@ -0,0 +1,42 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+using Winkellijst_ASP.Models;
+using System.Collections.ObjectModel;
+using Winkellijst_ASP.UnitOfWork;
+
+namespace NUnitTestWinkellijst
+{
+    [TestFixture]
+    public class UnitOfWorkTests
+    {
+        private IUnitOfWork unitOfWork = A.Fake<IUnitOfWork>();
+
+        [Test]
+
+        public void Ophalen_ReturnsObservableCollectionOfTypeProduct()
+        {
+            //Arrange
+            ObservableCollection<Product> Producten;
+            //Act
+            Producten = new ObservableCollection<Product>(unitOfWork.ProductRepo.Ophalen(x => x.Afdeling));
+            //Assert
+            Assert.NotNull(Producten);
+            Assert.IsInstanceOf<ObservableCollection<Product>>(Producten);
+        }
+
+        [Test]
+        public void ZoekOpPK_Returns1Reeks()
+        {
+            //Arrange
+            Product product = A.Fake<Product>();
+
+            //Act
+            product = unitOfWork.ProductRepo.ZoekOpPK(product.ProductId);
+
+            //Assert
+            Assert.NotNull(product);
+            Assert.IsInstanceOf<Product>(product);
+            A.CallTo(() => unitOfWork.ProductRepo.ZoekOpPK(product.ProductId)).MustHaveHappened();
+        }
+    }
+}
