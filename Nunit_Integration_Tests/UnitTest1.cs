26316ba7f5591049816e94419c5e9bc09ff5c394
Lennert Verdonck <r0792038@student.thomasmore.be>
2020-12-09 18:24:50 +0100

    UnitTesting

---
+++ Nunit_Integration_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using Winkellijst_ASP.Controllers;
+
+namespace Nunit_Integration_Tests
+{
+    public class Tests
+    {
+               
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
